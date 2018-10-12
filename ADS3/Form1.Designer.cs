c72045e0c9109fd125ddec00ce5ffc06d2e5854a
Михаил Дорышин <doryshin19@yandex.ru>
2018-10-12 10:28:42 +0500

    Добавьте файлы проекта.

---
+++ ADS3/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ADS3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How to enter data";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "From a file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(266, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(433, 360);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Manually";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "1. Enter the number of executers";
+            this.label2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(121, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "OK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "2. Enter the number of jobs";
+            this.label3.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(121, 206);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 27);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "OK";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 208);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Fill the matrix with lengths";
+            this.label4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(579, 395);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 32);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Accept";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 444);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "App";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ADS3/Form1.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ADS3
+{
+    public partial class Form1 : Form
+    {
+        int ex = 0;
+        int job = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.ReadOnly = true;
+            textBox1.ReadOnly = false;
+            label2.Visible = true;
+            label3.Visible = true;
+            for(int i = 0; i < ex; i++)
+            {
+                for(int j = 0; j < job; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = null;
+                }
+            }
+            for (int i = 0; i < ex; i++)
+            {
+                for (int j = 0; j < job; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = null;
+                }
+            }
+            dataGridView1.Visible = false;
+            label4.Visible = false;
+            button5.Visible = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Regex r = new Regex(@"[\d]+");
+            if (r.IsMatch(textBox1.Text) == true)
+            {
+                if (Convert.ToInt32(textBox1.Text) > 0)
+                {
+                    button3.Visible = true;
+                }
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ex = Convert.ToInt32(textBox1.Text);
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Regex r = new Regex(@"[\d]+");
+            if (r.IsMatch(textBox2.Text) == true)
+            {
+                if (Convert.ToInt32(textBox2.Text) > 0)
+                {
+                    button4.Visible = true;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            job = Convert.ToInt32(textBox2.Text);
+            textBox2.ReadOnly = true;
+            label4.Text = "Fill the matrix with lengths";
+            label4.Visible = true;
+            dataGridView1.Visible = true;
+            for(int i = 1; i <= job; i++)
+            {
+                dataGridView1.Columns.Add(i.ToString(), i.ToString());
+            }
+            for(int i = 1; i <= job; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i - 1].HeaderCell.Value = i;
+            }
+            button5.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.ReadOnly = true;
+            textBox1.ReadOnly = false;
+            label2.Visible = true;
+            label3.Visible = true;
+            for (int i = 0; i < job; i++)
+            {
+                for (int j = 0; j < job; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = null;
+                }
+            }
+            button3.Visible = false;
+            button4.Visible = false;
+            dataGridView1.Visible = false;
+            label4.Visible = false;
+            button5.Visible = false;
+            for(int i = 0; i < job; i++)
+            {
+                for(int j = 0; j < job; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = null;
+                }
+            }
+            label2.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            label3.Visible = false;
+            StreamReader Reader = new StreamReader("input.txt");
+            int countEmp = Convert.ToInt32(Reader.ReadLine());
+            string[] Emp = new string[countEmp];
+            List<Class1> main = new List<Class1>();// вершини с их глубиной, названиями других вершин которые в нее входят и выходят
+            {
+                string[] all = new string[2];
+                while (true)
+                {
+                    try
+                    {
+                        all = Reader.ReadLine().Split(' ');
+                    }
+                    catch { break; }
+                    bool ok1 = false, ok2 = false;
+                    for (int i = 0; i < main.Count; i++)// добавляет в список вершину и записывает в нее воходящие и выходящие
+                    {
+                        if (main[i].th == all[0])
+                        {
+                            main[i].outp.Add(all[1]);
+                            if (ok2 == true) break;
+                            ok1 = true;
+                        }
+                        else
+                            if (main[i].th == all[1])
+                        {
+                            main[i].into.Add(all[0]);
+                            if (ok1 == true) break;
+                            ok2 = true;
+                        }
+                    }
+                    if (ok1 == false) // если нет вершины добалвяет
+                    {
+                        main.Add(new Class1() { th = all[0] });
+                        main[main.Count - 1].outp.Add(all[1]);
+                    }
+                    if (ok2 == false)
+                    {
+                        main.Add(new Class1() { th = all[1] });
+                        main[main.Count - 1].into.Add(all[0]);
+                    }
+                }
+            }
+            Reader.Close();
+            {
+                List<int> treeWO = new List<int>();
+                for (int i = 0; i < main.Count; i++)
+                    if (main[i].outp.Count == 0) { treeWO.Add(i); main[i].deep = 0; }
+                foreach (int i in treeWO) //считает глубину
+                {
+                    foreach (string s in main[i].into)
+                        Fill(ref main, s, 1);
+                }
+            }
+            main.Sort(); //сортирует по глубине
+            int c = main[0].deep, stop = 0;
+            List<string> Done = new List<string>();
+            List<int> shouldBeD = new List<int>();
+            while (Done.Count < main.Count)
+            {
+                int t = 0;
+                List<string> doneInThisStep = new List<string>();
+                if (shouldBeD.Count != 0) //если есть те которые надо сделать, то делает
+                {
+                    for (int i = 0; i < shouldBeD.Count; i++)
+                    {
+                        if (t >= countEmp) break;
+                        if (main[shouldBeD[i]].Can(Done) == false) { Emp[t++] += main[shouldBeD[i]].th; doneInThisStep.Add(main[shouldBeD[i]].th); shouldBeD.RemoveAt(i); }
+                    }
+                }
+                if (t < countEmp) //заняты ли все рабочие
+                    for (int j = t; j < countEmp && stop < main.Count; j++)
+                    {
+                        while (stop < main.Count && main[stop].Can(Done)) shouldBeD.Add(stop++);
+                        if (stop != main.Count)
+                        {
+                            Emp[j] += main[stop].th;
+                            doneInThisStep.Add(main[stop++].th);
+                        }
+                    }
+                foreach (string s in doneInThisStep)
+                {
+                    Done.Add(s);
+                }
+            }
+            FileStream f = new FileStream("temp.txt",FileMode.OpenOrCreate);
+            StreamWriter w = new StreamWriter(f);
+            foreach (string s in Emp)
+            {
+                if (s != null)
+                {
+                    for (int i = 0; i < s.Length - 1; i++)
+                        w.Write("{0,3}", s[i]);
+                   w.WriteLine("{0,3}", s[s.Length - 1]);
+                }
+            }
+            w.Close();
+            f.Close();
+            StreamReader r = new StreamReader("temp.txt");
+            MessageBox.Show(r.ReadToEnd());
+            r.Close();
+            File.Delete("temp.txt");
+        }
+        static void Fill(ref List<Class1> main, string name, int c)
+        {
+            for (int i = 0; i < main.Count; i++)
+            {
+                if (main[i].th == name) if (main[i].deep < c)
+                    {
+                        main[i].deep = c++;
+                        foreach (string s in main[i].into)
+                            Fill(ref main, s, c);
+                    }
+            }
+        }
+        class Class1 : IComparable
+        {
+            public List<string> into = new List<string>();
+            public List<string> outp = new List<string>();
+            public string th;
+            public int deep;
+
+            public int CompareTo(object obj)
+            {
+                Class1 temp = (Class1)obj;
+                return deep.CompareTo(temp.deep) * -1;
+            }
+            public bool Can(List<string> Done)
+            {
+                int g = 0;
+                for (int i = 0; i < Done.Count; i++)
+                {
+                    if (into.Contains(Done[i])) g++;
+                    if (g == into.Count) return false;
+                }
+                if (g == into.Count) return false;
+                return true;
+            }
+        }
+        char[] names = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FileStream f1 = new FileStream("temp.txt", FileMode.OpenOrCreate);
+            StreamWriter w1 = new StreamWriter(f1);
+            w1.WriteLine(ex);
+            for(int i = 0; i < job; i++)
+            {
+                for(int j = 0; j < job; j++)
+                {
+                    if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) == 1)
+                    {
+                        w1.WriteLine(names[i] + " " + names[j]);
+                    }
+                }
+            }
+            w1.Close();
+            f1.Close();
+            StreamReader Reader = new StreamReader("temp.txt");
+            int countEmp = Convert.ToInt32(Reader.ReadLine());
+            string[] Emp = new string[countEmp];
+            List<Class1> main = new List<Class1>();// вершини с их глубиной, названиями других вершин которые в нее входят и выходят
+            {
+                string[] all = new string[2];
+                while (true)
+                {
+                    try
+                    {
+                        all = Reader.ReadLine().Split(' ');
+                    }
+                    catch { break; }
+                    bool ok1 = false, ok2 = false;
+                    for (int i = 0; i < main.Count; i++)// добавляет в список вершину и записывает в нее воходящие и выходящие
+                    {
+                        if (main[i].th == all[0])
+                        {
+                            main[i].outp.Add(all[1]);
+                            if (ok2 == true) break;
+                            ok1 = true;
+                        }
+                        else
+                            if (main[i].th == all[1])
+                        {
+                            main[i].into.Add(all[0]);
+                            if (ok1 == true) break;
+                            ok2 = true;
+                        }
+                    }
+                    if (ok1 == false) // если нет вершины добалвяет
+                    {
+                        main.Add(new Class1() { th = all[0] });
+                        main[main.Count - 1].outp.Add(all[1]);
+                    }
+                    if (ok2 == false)
+                    {
+                        main.Add(new Class1() { th = all[1] });
+                        main[main.Count - 1].into.Add(all[0]);
+                    }
+                }
+            }
+            Reader.Close();
+            {
+                List<int> treeWO = new List<int>();
+                for (int i = 0; i < main.Count; i++)
+                    if (main[i].outp.Count == 0) { treeWO.Add(i); main[i].deep = 0; }
+                foreach (int i in treeWO) //считает глубину
+                {
+                    foreach (string s in main[i].into)
+                        Fill(ref main, s, 1);
+                }
+            }
+            main.Sort(); //сортирует по глубине
+            int c = main[0].deep, stop = 0;
+            List<string> Done = new List<string>();
+            List<int> shouldBeD = new List<int>();
+            while (Done.Count < main.Count)
+            {
+                int t = 0;
+                List<string> doneInThisStep = new List<string>();
+                if (shouldBeD.Count != 0) //если есть те которые надо сделать, то делает
+                {
+                    for (int i = 0; i < shouldBeD.Count; i++)
+                    {
+                        if (t >= countEmp) break;
+                        if (main[shouldBeD[i]].Can(Done) == false) { Emp[t++] += main[shouldBeD[i]].th; doneInThisStep.Add(main[shouldBeD[i]].th); shouldBeD.RemoveAt(i); }
+                    }
+                }
+                if (t < countEmp) //заняты ли все рабочие
+                    for (int j = t; j < countEmp && stop < main.Count; j++)
+                    {
+                        while (stop < main.Count && main[stop].Can(Done)) shouldBeD.Add(stop++);
+                        if (stop != main.Count)
+                        {
+                            Emp[j] += main[stop].th;
+                            doneInThisStep.Add(main[stop++].th);
+                        }
+                    }
+                foreach (string s in doneInThisStep)
+                {
+                    Done.Add(s);
+                }
+            }
+            FileStream f = new FileStream("temp2.txt", FileMode.OpenOrCreate);
+            StreamWriter w = new StreamWriter(f);
+            foreach (string s in Emp)
+            {
+                if (s != null)
+                {
+                    for (int i = 0; i < s.Length - 1; i++)
+                        w.Write("{0,3}", s[i]);
+                    w.WriteLine("{0,3}", s[s.Length - 1]);
+                }
+            }
+            w.Close();
+            f.Close();
+            StreamReader r = new StreamReader("temp2.txt");
+            MessageBox.Show(r.ReadToEnd());
+            r.Close();
+            File.Delete("temp2.txt");
+            File.Delete("temp.txt");
+        }
+    }
+    }
+
